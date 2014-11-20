669cd304f00283ee61336d21c51f70846ab10df5
ghostFaceKillah <garmulewicz@gmail.com>
2014-11-20 23:52:45 +0100

    moved stuff to quaternion.h

--- quaternion.h
+++ quaternion.h
@@ -1,99 +1,186 @@
-#include <ostream> 
+#include<ostream> 
+#include<cmath>
 
 class Quaternion {
-        double re, i, j, k;
-    public:
-        // constructors
-        Quaternion();
-        /* pozwala na niejawne rzutowanie */
-        Quaternion(double re);
-        Quaternion(double re, double im);
-        Quaternion(double a, double b, double c, double d);
-        Quaternion(const Quaternion& q);
-        Quaternion(Quaternion&& q); 
+    double re, i, j, k;
+public:
+    // constructors
+    Quaternion() {
+        re = i = j = k = 0;
+    }
 
-        // operators
-        Quaternion operator+ ();
-        Quaternion operator- ();
-		friend Quaternion operator+(double x, const Quaternion& q);
-		//friend Quaternion operator+(const Quaternion& q, double x);
-		friend Quaternion operator-(double x, const Quaternion& q);
-		//friend Quaternion operator-(const Quaternion& q, double x);
+    Quaternion(double r) : re(r) {
+        i = j = k = 0;
+    }
 
-        // consider adding static designator to following operators
-        Quaternion& operator+=(const Quaternion& param);
-        Quaternion& operator-=(const Quaternion& param);
-        Quaternion& operator*=(const Quaternion& param);
-        //friend Quaternion operator*=(const Quaternion& q1, const Quaternion& q2);
-        Quaternion operator+ (const Quaternion& param);
-        Quaternion operator- (const Quaternion& param);
-        Quaternion operator* (const Quaternion& param);
-        Quaternion& operator= (const Quaternion& param);
-        Quaternion& operator= (Quaternion&& param);
-        //Quaternion& Quaternion::operator= (double x);
-        bool operator==(const Quaternion& other) const;
-        bool operator!=(const Quaternion& other) const;
+    Quaternion(double r, double im) : re(r), i(im) {
+        j = k = 0;
+    }
 
-        friend std::ostream& operator<<(std::ostream& out, const Quaternion& q);
+    constexpr Quaternion(double a, double b, double c, double d) : 
+        re(a), i(b), j(c), k(d) {};
 
-        // getters
-        double R() const;
-        double I() const;
-        double J() const;
-        double K() const;
-        double norm() const;
-        Quaternion conj();
-        explicit operator bool() const;
-        // (norma a + bi + cj + dk to sqrt(a*a + b*b + c*c + d*d));
+    Quaternion(const Quaternion& q2) = default;
+
+    Quaternion(Quaternion&& q) = default;
+
+    Quaternion& operator= (const Quaternion& param) = default;
+
+    Quaternion& operator= (Quaternion&& param) = default;
+
+    Quaternion operator+ () const {
+        Quaternion result = *this;
+        return result;
+    }
+
+    Quaternion operator- () const {
+        Quaternion result = *this;
+        result.re *= -1;
+        result.i *= -1;
+        result.j *= -1;
+        result.k *= -1;
+        return result;
+    }
+
+    Quaternion operator+ (const Quaternion& param) const {
+        Quaternion result = *this;
+        result += param;
+        return result;
+    }
+
+    Quaternion operator- (const Quaternion& param) const {
+        Quaternion result = *this;
+        result -= param;
+        return result;
+    }
+
+    Quaternion operator* (const Quaternion& param) const {
+        Quaternion result = *this;
+        result *= param;
+        return result;
+    }
+
+    const Quaternion& operator+=(const Quaternion& param) {
+        re += param.re;
+        i += param.i;
+        j += param.j;
+        k += param.k;
+        return *this;
+    }
+
+    const Quaternion& operator-=(const Quaternion& param) {
+        re -= param.re;
+        i -= param.i;
+        j -= param.j;
+        k -= param.k;
+        return *this;
+    }
+
+    const Quaternion& operator*=(const Quaternion& q) {
+
+        double new_re = re*q.R() - i*q.I() - j*q.J() - k*q.K();
+        double new_i = re*q.I() + i*q.R() + j*q.K() - k*q.J();
+        double new_j = re*q.J() - i*q.K() + j*q.R() + k*q.I();
+        double new_k = re*q.K() + i*q.J() - j*q.I() + k*q.R();
+        re = new_re;
+        i = new_i;
+        j = new_j;
+        k = new_k;
+        return *this;
+    }
+
+    // Quaternion & operator*=(const Quaternion & q) {
+    //     Quaternion qs = (*this) * q;
+    //     re =qs.R();
+    //         i = qs.I();
+    //         j = qs.J();
+    //         k = qs.K();
+
+    //     return (*this);
+    // }
+
+    bool operator==(const Quaternion& q) const {
+        return ((re == q.re) && (i == q.i) && (j == q.j) && (k == q.k));
+    }
+
+    bool operator!=(const Quaternion& q) const {
+        return !(*this == q);
+    }
+
+    // getters
+    double R() const {
+        return re;
+    }
+    double I() const {
+        return i;
+    }
+    double J() const {
+        return j;
+    }
+    double K() const {
+        return k;
+    }
+
+    double norm() const {
+        return sqrt(re*re + i*i + j*j + k*k);
+    }
+
+    inline const Quaternion conj() const {
+        return Quaternion (re, (-1)*i, (-1)*j, (-1)*k);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Quaternion& q);
+
+    explicit operator bool() const {
+        return !((re == 0) && (i == 0) && (j == 0) && (k == 0));
+    }
 };
 
 double norm(const Quaternion& q);
 Quaternion conj(const Quaternion& q);
 
-extern Quaternion const I;
-extern Quaternion const J;
-extern Quaternion const K;
+double norm(const Quaternion& q) {
+    return q.norm();
+}
 
-/* 
-* if(q), while(q) - warunek jest spełniony, gdy kwaternion nie jest
-równy zeru;
-* os << q - wypisuje kwaternion q na strumień os;
-* I - globalny niemodyfikowalny obiekt reprezentujący kwaternion (0, 1, 0, 0);
-* J - globalny niemodyfikowalny obiekt reprezentujący kwaternion (0, 0, 1, 0);
-* K - globalny niemodyfikowalny obiekt reprezentujący kwaternion (0, 0, 0, 1).
+Quaternion conj(const Quaternion& q) {
+    Quaternion tmp = q;
+    return tmp.conj();
+}
 
-Do reprezentacji składowych kwaternionu można użyć typu double.
+// operators
+Quaternion operator-(double x, const Quaternion& q) {
+    return Quaternion(x -q.R(), (-1)*q.I(), (-1)*q.J(), (-1)*q.K());
+}
 
-= Klasa QuaternionSequence =
-Klasa ta powinna implementować ciągi kwaternionów prawie wszędzie
-równe zeru. Powinny być dostępne następujące instrukcje:
-* QuaternionSequence qs - tworzy ciąg kwaternionów wszędzie równy zeru;
-* QuaternionSequence qs(m) - tworzy ciąg kwaternionów na podstawie mapy m,
-która jest typu std::map<QuaternionSequence::size_type, Quaternion>,
-klucze mapy oznaczają numery wyrazów w ciągu;
-* QuaternionSequence qs(v)- tworzy ciąg kwaternionów na podstawie wektora v,
-który jest typu std::vector<Quaternion>, kolejne elementy wektora
-oznaczają wartości wyrazów ciągu dla indeksów 0, 1, 2, ...;
-* QuaternionSequence qs2(qs1) - tworzy kopię ciągu kwaternionów qs1;
-* QuaternionSequence qs2 = qs1 - tworzy kopię ciągu kwaternionów qs1;
-* qs2 = qs1 - przypisuje ciąg kwaternionów qs1 na zmienną qs2;
-* qs1 += qs2 - dodaje do kolejnych wyrazów ciągu qs1 wyrazy ciągu qs2;
-* qs1 + qs2 - zwraca ciąg kwaternionów, którego kolejne wyrazy są
-* qs1 -= qs2 - odejmuje od kolejnych wyrazów ciągu qs1 wyrazy ciągu qs2;
-* qs1 - qs2 - zwraca ciąg kwaternionów, którego kolejne wyrazy są
-różnicami wyrazów z ciągów qs1 i qs2;
-* qs *= q - mnoży wszystkie wyrazy ciągu qs przez kwaternion q;
-* qs * q, q * qs - zwraca ciąg kwaternionów, którego kolejne wyrazy są
-wyrazami ciągu qs pomnożonymi przez kwaternion q;
-* qs[n] - zwraca kwaternion będący n-tym wyrazem ciągu qs (nie
-                                                           oznacza to możliwości przypisania qs[n] = ...);
-* insert(n, q) - wstawia kwaternion q na pozycję n;
-* qs1 == qs2 - zwraca true, wtw. gdy ciągi qs1 i qs2 są równe,
-tzn. mają równe kolejne wyrazy;
-* qs1 != qs2 - zwraca true wtw. gdy ciągi qs1 i qs2 są różne;
-* if(qs), while(qs) - warunek jest spełniony, gdy ciąg qs nie jest
-wszędzie równy zeru;
-* count() - zwraca liczbę aktualnie istniejących obiektów typu
-QuaternionSequence;
-* os << qs - wypisuje ciąg qs na strumień os.
-*/
+Quaternion operator-(const Quaternion& q, double x) {
+    return Quaternion(q.R() - x, q.I(), q.J(), q.K());
+}
+
+Quaternion operator+(double x, const Quaternion& q) {
+    return Quaternion(q.R() + x, q.I(), q.J(), q.K());
+}
+
+bool operator==(double x, const Quaternion& q) {
+    return (q == Quaternion (x));
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Quaternion& q) {
+    if (q) {
+        os << q.re;
+        if (q.i > 0) { os << "+"; }
+        if (q.i != 0) { os << q.i << "i"; }
+        if (q.j > 0) { os << "+"; }
+        if (q.j != 0) { os << q.j << "j"; }
+        if (q.k > 0) { os << "+"; }
+        if (q.k != 0) { os << q.k << "k"; }
+    } else { os << "0" << std::endl; }
+    return os;
+}
+
+
+constexpr Quaternion I{0, 1, 0, 0};
+constexpr Quaternion J{0, 0, 1, 0};
+constexpr Quaternion K{0, 0, 0, 1};
+
